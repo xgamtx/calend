f618ac8554dfbd70d8ce67beab711933e4175740
Wasiliy <wasiliyzaluew@MacBook-Pro-Wasiliy-2.local>
2014-02-02 11:03:07 +0400

    Вернул все

--- index.php
+++ index.php
@@ -22,6 +22,8 @@ $(document).ready(function(){
 </head>
  
 <body>
+<button>Start Animation</button>
+<p>By default, all HTML elements have a static position, and cannot be moved. To manipulate the position, remember to first set the CSS position property of the element to relative, fixed, or absolute!</p>
 <div id='a' style='border:1px solid #000;width:512px;position:relative'><div style='opacity:0.9;position:absolute;height:320px;width:0;background:#fff'></div>
 <img src='ss.png'>
 </div>
